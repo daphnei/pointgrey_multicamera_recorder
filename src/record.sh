6bd73167a36b9d3eac8c5dc11e3736a8b77a7431
Daphne Ippolito <daphnei@seas.upenn.edu>
2017-04-20 13:16:31 -0400

    Allow duration to be specified when calling record_unique_bag.launch

--- src/record.sh
+++ src/record.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+duration=$1
+
 # Save bag files with date as name to prevent overwriting.
-roslaunch poll_cameras record.launch out_file_prefix:=$(date "+%Y_%m_%d_%H_%M" )
+roslaunch poll_cameras record.launch out_file_prefix:=$(date "+%Y_%m_%d_%H_%M" ) duration:=$duration
  
